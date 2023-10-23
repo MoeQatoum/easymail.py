93ddc05174c1b0554f49b9a6cda19d95c8cdd41a
MoeQatoum <mhmd.qatoum@hotmail.com>
2023-10-23 19:21:46 +0400

    all emails stored in lower case

--- src/mailing_list.py
+++ src/mailing_list.py
@@ -1,4 +1,3 @@
-
 import re
 
 from file_handling import File
@@ -6,9 +5,9 @@ from common import W, Y, UL
 
 
 def is_email_addr(email: str) -> bool:
-    if match:= re.fullmatch("^[A-Za-z0-9._+\-\']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$", email):
+    if match := re.fullmatch("^[A-Za-z0-9._+\-']+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}$", email):
         return email == match.string
-    print(f"{Y}[WARN] \"{UL}{email}{W}{Y}\": does not match email pattern, skipping ...{W}")
+    print(f'{Y}[WARN] "{UL}{email}{W}{Y}": does not match email pattern, skipping ...{W}')
     return False
 
 
@@ -23,17 +22,17 @@ def get_mailing_list(raw_contacts: str, start_range: int = 1, end_range: int = 0
                 if email.startswith("#"):
                     continue
                 if is_email_addr(email):
-                    if not email in contacts:
-                        contacts.append(email)
+                    if not (email in contacts):
+                        contacts.append(email.lower())
         elif is_email_addr(i):
             if not i in contacts:
-                contacts.append(i)
+                contacts.append(i.lower())
 
     assert start_range >= 1, "start_range >= 1"
     end_range = len(contacts) if end_range == 0 or end_range > len(contacts) else end_range
     assert start_range <= end_range, "start_range <= end_range"
 
-    contacts = contacts[start_range-1:end_range]
+    contacts = contacts[start_range - 1 : end_range]
     print(f"[INFO] range: [{start_range} - {end_range}]")
     print(f"[INFO] Emails Count: {len(contacts)}")
     return contacts
